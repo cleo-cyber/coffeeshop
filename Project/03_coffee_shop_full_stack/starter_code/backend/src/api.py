f1798564f733bd65fb70e91966c7316e3850c7c6
cleo-cyber <kadimacleo92@gmail.com>
2022-06-17 07:38:46 +0300

    submission

--- Project/03_coffee_shop_full_stack/starter_code/backend/src/api.py
+++ Project/03_coffee_shop_full_stack/starter_code/backend/src/api.py
@@ -99,8 +99,10 @@ def post_drinks(payload):
 
        
     except:
-        # db.session.rollback()
+        db.session.rollback()
         abort(422)
+    finally:
+        db.session.close()
  
      
 '''
